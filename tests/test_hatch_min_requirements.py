97bfd2df3ee8ca97697f41c26aa540b790bbb372
Talley Lambert <talley.lambert@gmail.com>
2024-02-05 16:59:49 -0500

    add test

--- tests/test_hatch_min_requirements.py
+++ tests/test_hatch_min_requirements.py
@@ -1,6 +1,29 @@
-SPECIFIERS = [
-    "~= 2.2",
-    ">= 2.2, == 2.*",
-    "~= 1.4.5",
-    ">= 1.4.5, == 1.4.*",
+import pytest
+
+from hatch_min_requirements.util import sub_min_compatible_version
+
+# these may change if the min versions change upstream
+PARAMS = [
+    ("numpy", "numpy==1.3.0", "numpy"),
+    ("numpy!=1.3.6", "numpy==1.3.0", "numpy!=1.3.6"),
+    ("numpy!=1.3.0", "numpy==1.4.1", "numpy!=1.3.0"),
+    ("numpy~=1.7", "numpy==1.7.0", "numpy==1.7"),
+    ("numpy>=1.5.0", "numpy==1.5.0", "numpy==1.5.0"),
+    ("numpy[extra]", "numpy[extra]==1.3.0", "numpy[extra]"),
+    ("numpy>1.3", "numpy==1.4.1", "numpy>1.3"),
+    ("numpy[extra]>1.3", "numpy[extra]==1.4.1", "numpy[extra]>1.3"),
+    (
+        "numpy[extra]>1.3; python_version=='3.7'",
+        "numpy[extra]==1.4.1; python_version=='3.7'",
+        "numpy[extra]>1.3; python_version=='3.7'",
+    ),
 ]
+
+
+@pytest.mark.parametrize("offline", [True, False])
+@pytest.mark.parametrize("package, online_expectation, offline_expectation", PARAMS)
+def test_sub_min_compatible_version(
+    package: str, online_expectation: str, offline_expectation: str, offline: bool
+) -> None:
+    expectation = offline_expectation if offline else online_expectation
+    assert sub_min_compatible_version(package, offline=offline) == expectation
